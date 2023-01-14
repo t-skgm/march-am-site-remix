ff1e1f8f698d75fc4436404dbc6c7ae4e15aba03
t-skgm <skgm0301@gmail.com>
2023-01-15 00:29:25 +0900

    cf config

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,5 +1,5 @@
 import { PassThrough } from "stream";
-import type { EntryContext } from "@remix-run/node";
+import type { EntryContext } from "@remix-run/cloudflare";
 import { Response } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import isbot from "isbot";
