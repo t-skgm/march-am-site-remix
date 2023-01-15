a80d3c85b01a7030c5a0df5af189529d1d3379fc
t-skgm <skgm0301@gmail.com>
2023-01-15 20:11:43 +0900

    some build conf

--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,7 @@
  */
 module.exports = {
   serverBuildTarget: "cloudflare-workers",
+  server: "./server.ts",
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
 };
