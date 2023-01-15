04d9d29f213f74168e51edeaf46253d17137d321
t-skgm <skgm0301@gmail.com>
2023-01-15 20:17:28 +0900

    fix

--- remix.config.js
+++ remix.config.js
@@ -6,4 +6,8 @@ module.exports = {
   server: "./server.ts",
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  serverBuildPath: "build/index.js",
+  publicPath: "/build/",
 };
